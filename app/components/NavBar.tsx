f828c87a784591dae93fa17964023b301de8ca98
Deekshant Yadav <deeksh4nt@gmail.com>
2024-01-01 19:25:38 +0530

    created global navbar

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,19 @@
+interface Props {
+  title: string;
+  itemCount: number;
+}
+
+export default function NavBar({ title, itemCount }: Props) {
+  return (
+    <>
+    <div className="w-full px-4 py-3 fixed flex items-center justify-between bg-blue-600 text-white shadow-md">
+      <h1 className="text-xl font-semibold">{title}</h1>
+      <a href="/cart" className="relative flex justify-center items-center">
+        { itemCount > 0 && <p className="absolute top-[-4px] right-[-4px] text-xs font-semibold bg-rose-600 text-white rounded-lg px-1 min-w-4 min-h-4 flex justify-center items-center">{itemCount}</p>}
+        <span className="material-symbols-outlined">shopping_cart</span>
+      </a>
+    </div>
+    <div className="w-full px-4 py-3 text-lg">White Space</div>
+    </>
+  )
+}
