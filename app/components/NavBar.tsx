de4a6aab6ad0d2054c78fab9628bd6dddfbe04fd
Deekshant Yadav <deeksh4nt@gmail.com>
2024-01-03 19:13:18 +0530

    replaced a tag with next/Link

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link"
+
 interface Props {
   title: string;
   itemCount: number;
@@ -8,10 +10,10 @@ export default function NavBar({ title, itemCount }: Props) {
     <>
     <div className="w-full px-4 py-3 fixed flex items-center justify-between bg-blue-600 text-white shadow-md">
       <h1 className="text-xl font-semibold">{title}</h1>
-      <a href="/cart" className="relative flex justify-center items-center">
+      <Link href="/cart" className="relative flex justify-center items-center">
         { itemCount > 0 && <p className="absolute top-[-4px] right-[-4px] text-xs font-semibold bg-rose-600 text-white rounded-lg px-1 min-w-4 min-h-4 flex justify-center items-center">{itemCount}</p>}
         <span className="material-symbols-outlined">shopping_cart</span>
-      </a>
+      </Link>
     </div>
     <div className="w-full px-4 py-3 text-lg">White Space</div>
     </>
